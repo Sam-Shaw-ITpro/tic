09f58c0e69a8a57c8108ad1757b8ac1dfc0817b6
Sam Shaw <sam.shaw.itpro@gmail.com>
2019-02-16 13:41:50 -0500

    more

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ModuleWithComponentFactories } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic';
-  blank = '-';
+  blank = ` `;
   x = 'X';
   o = "O";
   one = this.blank;
@@ -20,140 +20,126 @@ export class AppComponent {
   eight = this.blank;
   nine = this.blank;
   player = this.x;
+  oneortwo = 2;
+  info = ""
 
-  currentPlayer() : void {
+  winner(): void {
+    // if winner make an alert or change the BGcolor of winning block
+    if (this.one == this.player && this.two == this.player && this.three == this.player)
+      console.log("123winner");
+    else if (this.four == this.player && this.five == this.player && this.six == this.player)
+      console.log("456winner");
+    else if (this.seven == this.player && this.eight == this.player && this.nine == this.player)
+      console.log("789winner");
+    else if (this.one == this.player && this.four == this.player && this.seven == this.player)
+      console.log("147winner");
+      else if (this.two == this.player && this.five == this.player && this.eight == this.player)
+      console.log("258winner");
+      else if (this.three == this.player && this.six == this.player && this.nine == this.player)
+      console.log("369winner");
+      else if (this.one == this.player && this.five == this.player && this.nine == this.player)
+      console.log("159winner");
+      else if (this.three == this.player && this.five == this.player && this.seven == this.player)
+      console.log("357winner");
+else 
+console.log("NO WINNER");
+  }
+
+  gameOver(): void {
+    console.log("WINNER");
+    //lock game???
+  }
+
+  currentPlayer(): void {
     if (this.player == this.x)
-    this.player = this.o;
-    else 
-    this.player = this.x;
+      this.player = this.o;
+    else
+      this.player = this.x;
+  }
+
+  numOfPlayers(): void {
+    if (this.oneortwo == 1)
+      this.oneortwo = 2;
+    else
+      this.oneortwo = 1;
+    this.playcomputer();
+  }
+
+  playcomputer(): void {
+    console.log("play the computer")
+    this.info = "Playing the computer, make your first move, then the computer will go"
   }
 
   changeOne(): void {
     if (this.one == this.blank) {
       this.one = this.player;
+      this.winner();
       this.currentPlayer();
-      // console.log('num ', this.one);
-    // } else if (this.one == this.x) {
-      // this.one = this.o;
-      // console.log('num ', this.one);
-    // } else if (this.one == this.o) {
-      // this.one = this.blank;
-      // console.log('num ', this.one);
-    // };
-    // console.log('num ', this.one);
+    }
   }
-}
 
   changeTwo(): void {
     if (this.two == this.blank) {
       this.two = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.two == this.x) {
-    //   this.two = this.o;
-    //   console.log('num ', this.two);
-    // } else if (this.two == this.o) {
-    //   this.two = this.blank;
-    //   console.log('num ', this.two);
     };
-    // console.log('num ', this.two);
   }
 
   changeThree(): void {
     if (this.three == this.blank) {
       this.three = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.three == this.x) {
-    //   this.three = this.o;
-    //   console.log('three ', this.three);
-    // } else if (this.three == this.o) {
-    //   this.three = this.blank;
-    //   console.log('three ', this.three);
     };
-    // console.log('three ', this.three);
   }
 
   changeFour(): void {
     if (this.four == this.blank) {
       this.four = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.four == this.x) {
-    //   this.four = this.o;
-    //   console.log('four ', this.four);
-    // } else if (this.four == this.o) {
-    //   this.four = this.blank;
-    //   console.log('four ', this.four);
     };
-    // console.log('four ', this.four);
   }
 
   changeFive(): void {
     if (this.five == this.blank) {
       this.five = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.five == this.x) {
-    //   this.five = this.o;
-    //   console.log('five ', this.five);
-    // } else if (this.five == this.o) {
-    //   this.five = this.blank;
-    //   console.log('five ', this.five);
     };
-    // console.log('five ', this.five);
   }
 
   changeSix(): void {
     if (this.six == this.blank) {
       this.six = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.six == this.x) {
-    //   this.six = this.o;
-    //   console.log('six ', this.six);
-    // } else if (this.six == this.o) {
-    //   this.six = this.blank;
-    //   console.log('six ', this.six);
     };
-    // console.log('six ', this.six);
   }
 
   changeSeven(): void {
     if (this.seven == this.blank) {
       this.seven = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.seven == this.x) {
-    //   this.seven = this.o;
-    //   console.log('seven ', this.seven);
-    // } else if (this.seven == this.o) {
-    //   this.seven = this.blank;
-    //   console.log('seven ', this.seven);
     };
-    // console.log('seven ', this.seven);
   }
 
   changeEight(): void {
     if (this.eight == this.blank) {
       this.eight = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.eight == this.x) {
-    //   this.eight = this.o;
-    //   console.log('eight ', this.eight);
-    // } else if (this.eight == this.o) {
-    //   this.eight = this.blank;
-    //   console.log('eight ', this.eight);
     };
-    // console.log('eight ', this.eight);
   }
 
   changeNine(): void {
     if (this.nine == this.blank) {
       this.nine = this.player;
+      this.winner();
       this.currentPlayer();
-    // } else if (this.nine == this.x) {
-    //   this.nine = this.o;
-    //   console.log('nine ', this.nine);
-    // } else if (this.nine == this.o) {
-    //   this.nine = this.blank;
-    //   console.log('nine ', this.nine);
     };
-    // console.log('nine ', this.nine);
   }
 
   resetGame() {
@@ -167,6 +153,7 @@ export class AppComponent {
     this.eight = this.blank;
     this.nine = this.blank;
     this.player = this.x;
+    this.oneortwo = 2;
+    this.info = '';
   }
-
 }
